29da787798f5d0eb5f3e01a38aa558691cdc811e
ShineYu <shine@wivlabs.com>
2020-03-20 17:21:26 +0900

    commit

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,6 +1,4 @@
 import React, { ReactNode } from 'react';
-import {getDirection} from '../helpers/Utils';
-
 
 interface Props {
   children : ReactNode
@@ -9,14 +7,7 @@ interface Props {
 export default class App extends React.Component<Props> {
   constructor(props:Props){
     super(props);
-    const direction = getDirection();
-        if (direction.isRtl) {
-            document.body.classList.add('rtl');
-            document.body.classList.remove('ltr');
-        } else {
-            document.body.classList.add('ltr');
-            document.body.classList.remove('rtl');
-        }
+
   }
   render() {
 
